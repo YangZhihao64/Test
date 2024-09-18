941a22748d7903666bc927e7b786d86a0f9da7ab
Zhihao Yang <1401134592@qq.com>
2024-09-18 21:37:20 +0800

    字符串学习完成

--- test.py
+++ test.py
@@ -1,6 +1,27 @@
+import re
+
 print ("hello world")
 print ("First Change")
+'''
 firstname = input("Enter your first name: ")
 lastname = input("Enter your last name: ")
-fullname = firstname + "," + lastname
-print(fullname)
+
+fullname = firstname[0] + lastname[0]
+
+print (fullname)
+'''
+Sentence = "good morning my neighbors"
+Number = 2
+Float = 5.2
+
+print ("I say : %s, Integer is : %d , Float is : %.1f"%(Sentence, Number, Float))
+
+#X = re.split("\s", Sentence, 2)
+#X = re.sub("\s","0",Sentence)
+
+#print(Sentence.upper())
+#print(Sentence.lower())
+
+#print ("no space on both side: " + Sentence.strip())
+#print ("no hello in sentence: " + Sentence.lstrip("hello"))
+#print ("no world in sentence: " + Sentence.rstrip("world"))
