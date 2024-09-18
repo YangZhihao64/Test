ab0fc5fa1d485b8404220fdddbb94160e5a5b4d5
Zhihao Yang <1401134592@qq.com>
2024-09-18 20:40:14 +0800

    字符串学习

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
 print ("hello world")
 print ("First Change")
+firstname = input("Enter your first name: ")
+lastname = input("Enter your last name: ")
+fullname = firstname + "," + lastname
+print(fullname)
