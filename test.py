748253196dcceeb244921247a9b5fc0e9f3c7968
Zhihao Yang <1401134592@qq.com>
2024-09-22 10:34:20 +0100

    函数学习

--- test.py
+++ test.py
@@ -1,20 +1,20 @@
 import re
 
-print ("hello world")
-print ("First Change")
-'''
-firstname = input("Enter your first name: ")
-lastname = input("Enter your last name: ")
-
-fullname = firstname[0] + lastname[0]
-
-print (fullname)
-'''
-Sentence = "good morning my neighbors"
-Number = 2
-Float = 5.2
-
-print ("I say : %s, Integer is : %d , Float is : %.1f"%(Sentence, Number, Float))
+# print ("hello world")
+# print ("First Change")
+# '''
+# firstname = input("Enter your first name: ")
+# lastname = input("Enter your last name: ")
+#
+# fullname = firstname[0] + lastname[0]
+#
+# print (fullname)
+# '''
+# Sentence = "good morning my neighbors"
+# Number = 2
+# Float = 5.2
+#
+# print ("I say : %s, Integer is : %d , Float is : %.1f"%(Sentence, Number, Float))
 
 #X = re.split("\s", Sentence, 2)
 #X = re.sub("\s","0",Sentence)
@@ -25,3 +25,21 @@ print ("I say : %s, Integer is : %d , Float is : %.1f"%(Sentence, Number, Float)
 #print ("no space on both side: " + Sentence.strip())
 #print ("no hello in sentence: " + Sentence.lstrip("hello"))
 #print ("no world in sentence: " + Sentence.rstrip("world"))
+
+
+def add(x,y = 5):
+    s = x+y
+    print(s)
+
+
+add(3,3)
+
+
+
+
+
+
+
+
+
+
